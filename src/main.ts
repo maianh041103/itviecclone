42c474a7117a84ecd9c9547b1c98c18fcf05332c
maianh041103 <nguyenmaianh041103@gmail.com>
2024-04-22 22:39:15 +0700

    Fix cors

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
 
   //CORS
   app.enableCors({
-    origin: true,
+    origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
