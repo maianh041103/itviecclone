c2e3061063f9acb95dfd10e06c8a8a82f0048bc4
maianh041103 <nguyenmaianh041103@gmail.com>
2024-04-23 01:44:29 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
 
   //CORS
   app.enableCors({
-    origin: "*",
+    origin: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
